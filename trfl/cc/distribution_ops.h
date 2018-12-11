8fa2581a9b1991d58968525359183878e3a0e739
diegolascasas <diegolascasas@google.com>
2018-12-11 15:16:03 +0000

    Add the license headers for C++ ops.
    PiperOrigin-RevId: 224997375

--- trfl/cc/distribution_ops.h
+++ trfl/cc/distribution_ops.h
@@ -1,5 +1,19 @@
-#ifndef LEARNING_DEEPMIND_RL_LEARNING_CC_DISTRIBUTION_OPS_H_
-#define LEARNING_DEEPMIND_RL_LEARNING_CC_DISTRIBUTION_OPS_H_
+/* Copyright 2018 The trfl Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#ifndef TRFL_CC_DISTRIBUTION_OPS_H_
+#define TRFL_CC_DISTRIBUTION_OPS_H_
 
 #include <algorithm>
 #include <functional>
@@ -40,4 +54,4 @@ class ProjectDistribution : public tensorflow::OpKernel {
 }  // namespace rl
 }  // namespace deepmind
 
-#endif  // LEARNING_DEEPMIND_RL_LEARNING_CC_DISTRIBUTION_OPS_H_
+#endif  // TRFL_CC_DISTRIBUTION_OPS_H_
